538ff729f6eaefc977ff8edb3ec7db9b1c8f657f
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-12 21:49:23 +0000

    moved welcome.css to separate file

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
 
 <table class="table">
--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,12 @@
 
 //default route/page
 Route::get('/', function () {
-    //return view('welcome');
-
-    //check if usr logged in
-    /*if(Auth::check()){
-        return "the user is logged in";
-    }*/
-
-    //not for use here but eredirect user to page they were going to
-    /*if(Auth::attempt('username'=>$username,'password'=>$password)){
-        return redirect()->intended();
-    }*/
-
+    return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+//Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
